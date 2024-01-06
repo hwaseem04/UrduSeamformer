46163176446e7225b97ed869c1ccc5e346fe5ff5
hwaseem04 <hajawaseem4@gmail.com>
2024-01-06 07:06:48 +0530

    modify data patches preparation file to prepare only scribble patches

--- datapreparation.py
+++ datapreparation.py
@@ -39,8 +39,6 @@ def argumentParser():
     parser.add_argument('--datafolder',type=str,default=None)
     parser.add_argument('--patchsize',type=int,default=PATCHSIZE)
     parser.add_argument('--overlap',type=float,default=OVERLAP)
-    parser.add_argument('--binaryFlag',type=bool,default=True)
-    parser.add_argument('--binaryFolderPath',type=str,default=None)
     parser.add_argument('--mode',type=str,default='train')
     parser.add_argument('--outputfolderPath',type=str,default=None,required=True)
     args = parser.parse_args()
@@ -52,68 +50,21 @@ def argumentParser():
 def createFolders(args):
     os.makedirs(args.outputfolderPath, exist_ok=True)
     smPath = os.path.join(args.outputfolderPath,'scribbleMap/')
-    if args.binaryFlag:
-        bmPath = os.path.join(args.outputfolderPath,'binaryImages/')
     imPath = os.path.join(args.outputfolderPath,'images/')
     # Prepare a key point image folder 
     try : 
         os.makedirs(smPath,exist_ok=True)
-        if args.binaryFlag:
-            os.makedirs(bmPath,exist_ok=True)
         os.makedirs(imPath,exist_ok=True)
     except FileExistsError: 
         print('Error in Folder Creation !')
     print('~Folder Creation Completed !')
 
-# Binarisation technique 
-def sauvola_niblack_threshold(image,window_size=7):
-    h,w,c=image.shape
-    if(c>1):
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    binary_global = image > threshold_otsu(image)
-    thresh_niblack = threshold_niblack(image, window_size=window_size, k=0.8)
-    thresh_sauvola = threshold_sauvola(image, window_size=window_size)
-    binary_niblack = image > thresh_niblack
-    binary_sauvola = image > thresh_sauvola
-    return(binary_sauvola,binary_niblack)
-
-# Cleaning while binarisation
-def cleanImage(img):
-    _, blackAndWhite = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
-    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(blackAndWhite, None, None, None, 8, cv2.CV_32S)
-    sizes = stats[1:, -1] #get CC_STAT_AREA component
-    img2 = np.zeros((labels.shape), np.uint8)
-    for i in range(0, nlabels - 1):
-        if sizes[i] >= 30:   #filter small dotted regions
-            img2[labels == i + 1] = 255
-    res = cv2.bitwise_not(img2)
-    return res 
-
-# Preproceesing : Binarisation of documents 
-def preprocess(image):
-    bs,bn = sauvola_niblack_threshold(image,window_size=15)
-    bs = bs * 255 
-    bn = bn * 255 
-    # Reshape it 
-    bs = bs.reshape((bs.shape[0],bs.shape[1],1))    
-    bs =np.uint8(bs)
-    clean_image=255-cleanImage(bs)
-    # Copy it to 3 channels..
-    clean_image = np.stack((clean_image,) * 3, axis=-1)
-    return(clean_image)
 
 # Drawing scribble on canvas
 def drawScribble(canvas,scribble, thickness=THICKNESS):
     canvas=cv2.polylines(canvas,np.int32([scribble]),False,(255,255,255),thickness)
     return canvas
 
-# Binarisation Function Call 
-def get_channel_binary(image):
-    binImage = preprocess(image)
-    graybinImage = cv2.cvtColor(binImage ,cv2.COLOR_BGR2GRAY)
-    graybinImage = graybinImage/255 
-    graybinImage = np.asarray(graybinImage,dtype=np.int32)
-    return graybinImage
 
 # Scribble Map Generation 
 def get_channel_scibbles(img,scribbleList,thickness=THICKNESS):
@@ -145,7 +96,6 @@ def datasetPrepare(args):
         print('Processing .. {}'.format(path))
         img = cv2.imread(path)
         imgName=os.path.basename(path)
-        lower_path = path.lower()
 
         try:
             if  'scribbles' not in datapoint:
@@ -164,24 +114,13 @@ def datasetPrepare(args):
             spatches,indices = emp.extract_patches(sMap,patchsize=args.patchsize,overlap=args.overlap)
             ipatches,indices = emp.extract_patches(img,patchsize=args.patchsize,overlap=args.overlap)
 
-            if args.binaryFlag:
-                # Either get the binary image via Sauvola-Niblack Binarisation Method
-                bMap = get_channel_binary(img) * 255 # patches --> White text, black background
-                if args.binaryFolderPath is not None : 
-                    binImage = 255 - cv2.imread(os.path.join(args.binaryFolderPath,imgName.replace('.jpg', '_binarized.jpg')))
-                    graybinImage = cv2.cvtColor(binImage ,cv2.COLOR_BGR2GRAY)
-                    bMap = np.asarray(graybinImage,dtype=np.int32) 
-
-                # Go ahead and compute patches 
-                bpatches,indices = emp.extract_patches(bMap,patchsize=args.patchsize,overlap=args.overlap)
             N = len(spatches)
             for i in range(0,N,1):
                 count = count + 1
                 # Resizing of the patches to 256 x 256 pixels
                 ipatch=  cv2.resize(ipatches[i], (args.patchsize,args.patchsize), interpolation = cv2.INTER_AREA)
                 spatch=  cv2.resize(spatches[i], (args.patchsize,args.patchsize), interpolation = cv2.INTER_AREA)
-                if args.binaryFlag:
-                    bpatch = cv2.resize(bpatches[i], (args.patchsize,args.patchsize), interpolation = cv2.INTER_AREA)
+
                 # List of indices to name the patch
                 lindices = list(indices[i])
                 imageName_i = imgName.split('.')[0]+'_{}_{}_{}_{}'.format(str(lindices[0]),str(lindices[1]),str(lindices[2]),str(lindices[3]))
@@ -189,8 +128,6 @@ def datasetPrepare(args):
                     # Save the image patch to respective folders
                     cv2.imwrite(os.path.join(args.outputfolderPath,'scribbleMap/sm_{}.jpg'.format(imageName_i)),spatch)
                     cv2.imwrite(os.path.join(args.outputfolderPath,'images/im_{}.jpg'.format(imageName_i)),ipatch)
-                    if args.binaryFlag:
-                        cv2.imwrite(os.path.join(args.outputfolderPath,'binaryImages/bm_{}.jpg'.format(imageName_i)),bpatch)
                 except Exception as exp:
                     print('Error : Saving the patch {}'.format(exp))
                     errors+=1
