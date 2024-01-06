1540345e35c0c8467fb608fe02bb264471f2b2cf
hwaseem04 <hajawaseem4@gmail.com>
2024-01-06 07:09:04 +0530

    modify inference script to appropriately use input as binaryMap

--- inference.py
+++ inference.py
@@ -24,6 +24,7 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # File Imports 
 from seam_conditioned_scribble_generation import *
 from utils import *
+import utils_urdu as pu
 from network import *
 
 # Argument Parser 
@@ -109,11 +110,9 @@ def imageInference(network,path,args,PDIM=256,DIM=256,OVERLAP=0.25,save=True):
         sys.exit(0)
     else:
         weight = torch.tensor(1) #Dummy weight 
-        parentImage=cv2.imread(path)
         input_patches , indices = readFullImage(path,PDIM,DIM,OVERLAP)
 
         patch_size=args['patch_size']
-        img_size = args['img_size']
         spilt_size = args['split_size']
         image_size = (spilt_size,spilt_size)
         THRESHOLD = args['threshold']
@@ -128,101 +127,90 @@ def imageInference(network,path,args,PDIM=256,DIM=256,OVERLAP=0.25,save=True):
                 inputs =torch.from_numpy(p).to(device)
                 # Pass through model
                 loss_criterion = torch.nn.BCEWithLogitsLoss(pos_weight=weight, reduction='none')
-                pred_pixel_values_bin,pred_pixel_values_scr=network(inputs,gt_bin_img=inputs,gt_scr_img=inputs,criterion=loss_criterion,strain=True,btrain=True,mode='test')
+                pred_pixel_values_scr=network(inputs, gt_scr_img=inputs,criterion=loss_criterion, mode='test')
 
-                # Send them to .cpu
-                pred_pixel_values_bin = pred_pixel_values_bin.cpu()
+                # Send to .cpu
                 pred_pixel_values_scr = pred_pixel_values_scr.cpu()
-
-                bpatch=reconstruct(pred_pixel_values_bin,patch_size,target_shape,image_size)
                 spatch=reconstruct(pred_pixel_values_scr,patch_size,target_shape,image_size)
 
                 # binarize the predicted image taking 0.5 as threshold
-                bpatch = ( bpatch>THRESHOLD)*1
                 spatch = ( spatch>THRESHOLD)*1
 
                 # Append the net processed patch
                 soutput_patches.append(255*spatch)
-                boutput_patches.append(255*bpatch)
 
         try:
-            assert len(boutput_patches)==len(soutput_patches)==len(input_patches)
+            assert len(soutput_patches)==len(input_patches)
         except Exception as exp:
             print('Error in patch processing outputs : Exiting!')
             sys.exit(0)
         
         # Restich the image
         soutput = emp.merge_patches(soutput_patches,indices,mode='max')
-        boutput = emp.merge_patches(boutput_patches,indices,mode='max')
 
         # Transpose
-        binaryOutput=np.transpose(boutput,(1,0))
         scribbleOutput=np.transpose(soutput,(1,0))
         
-        return binaryOutput,scribbleOutput
+        return scribbleOutput
     
 '''
 Post Processing Function
 '''
-def postProcess(scribbleImage,binaryImage,binaryThreshold=40,rectangularKernel=30):
-    scr = np.repeat(scribbleImage[:, :, np.newaxis], 3, axis=2) 
-    bin = np.repeat(binaryImage[:, :, np.newaxis], 3, axis=2)
+def postProcess_URDU(scr, bin, thresh=40, rectangularKernel=30):
+    scr = np.repeat(scr[:, :, np.newaxis], 3, axis=2) 
+    bin = np.repeat(bin[:, :, np.newaxis], 3, axis=2)
+
     bin = bin.astype(np.uint8)
     scr = scr.astype(np.uint8)
-    # print('PP @ BIN SHAPE : {} SCRIBBLE SHAPE : {}'.format(scribbleImage.shape,binaryImage.shape))
-    # bin_ = cv2.cvtColor(bin_,cv2.COLOR_BGR2GRAY)
-    H,W,_ = bin.shape
-    mask_with_contours=copy.deepcopy(bin)
 
-    # We apply distance transform to thin the output polygon
-    
-    tmp = polygon_to_distance_mask(scr,threshold=30)
+    tmp = pu.polygon_to_distance_mask(scr,threshold=30)
     final_tmp = np.zeros_like(scr)
     for j in range(3):
         final_tmp[:, :, j] = tmp
     scr = final_tmp
-    # Threshold it
-    bin[bin>=binaryThreshold]=255
-    bin[bin<binaryThreshold]=0
-    scr[scr>=binaryThreshold]=255
-    scr[scr<binaryThreshold]=0
+    bin[bin>=thresh]=255
+    bin[bin<thresh]=0
+    scr[scr>=thresh]=255
+    scr[scr<thresh]=0
 
-    # Bitwise AND of the textual region and polygon region ( only cut off letters will be highlighted)
+    # Usage of binarization results
     scr_ = cv2.bitwise_and(bin/255,scr/255)
-    # Dilate the existing text content 
-    # scr_ = text_dilate(scr_,kernel_size=3,iterations=3) # SD = 3,3 
-    scr_ = text_dilate(scr_,kernel_size=3,iterations=3) # KH 3,7
+    scr_ = pu.dilate_image(scr_,kernel_size=3,iterations=2)
+    scr_ = pu.horizontal_dilation(scr_,rectangularKernel,1)
 
-    # Dilate it horizontally to fill the gaps within the text region 
-    # scr_ = horizontal_dilation(scr_,rectangularKernel,3) # SD - 50 ,3 
-    scr_ = horizontal_dilation(scr_,rectangularKernel,1) # KH - 50 ,2 
-   
-    # Extract the final contours 
-    contours = cleanImageFindContours(scr_,threshold = 0.10)
+    box_img, box, cut = pu.find_text_bounding_box(scr_*255)
 
-    # Combine the hulls that are on the same horizontal level 
-    new_hulls = combine_hulls_on_same_level(contours, tolerance=30)
-    
-    # Scribble Generation
-    predictedScribbles=[]
+    scr_ = pu.get_subset(box, scr_)
+
+    # temporary contours
+    contours = pu.cleanImageFindContours(scr_,threshold = 0.10)
+
+    H,W,C = bin.shape
+    mask_with_contours=copy.deepcopy(bin)
+
+    # Hull operation - merge contours
+    _,new_hulls = pu.combine_hulls_on_same_level(contours, threshold=30)
+    genScribbles=[]
 
     for c in new_hulls  :
         canvas_copy = np.zeros(scr_.shape)
         c = np.asarray(c,dtype=np.int32).reshape((-1,1,2))
         canvas_copy = cv2.fillPoly(canvas_copy,np.int32([c]),(255,255,255))
 
-        contours = cleanImageFindContours(canvas_copy,threshold = 0.10)
+        canvas_copy = pu.horizontal_dilation(canvas_copy,cut + cut//2,1)
+
+        contours = pu.cleanImageFindContours(canvas_copy,threshold = 0.10)
         h=np.asarray(contours[0],dtype=np.int32)
         h = cv2.convexHull(h)
         h=np.asarray(h,dtype=np.int32).reshape((-1,2))
         h=h.tolist()
-        scr = generateScribble(H,W,h)
+        scr = pu.generateScribble(H,W,h)
         scr_arr = np.asarray(scr,dtype=np.int32).reshape((-1,1,2))
         mask_with_contours=cv2.polylines(mask_with_contours,[scr_arr], isClosed=False, color=(0,255,0),thickness=3)
 
         scr_lst = scr_arr.reshape((-1,2)).tolist()
-        predictedScribbles.append(scr_lst)
-    return predictedScribbles
+        genScribbles.append(scr_lst)
+    return genScribbles
 
 '''
 Performs Binary & Scribble 
@@ -245,60 +233,6 @@ def Inference(args):
     os.makedirs(bin_folder,exist_ok=True)
     os.makedirs(vis_folder,exist_ok=True)
 
-    evalDict = {"IoU": [], "HD": [], "AvgHD": [], "HD95": []}
-
-    # Case I : Input JSON 
-    if args['input_image_json'] is not None and os.path.exists(args['input_image_json']):
-        # Read and open the input json file 
-        with open(args['input_image_json'], "r") as json_file:
-            data = json.load(json_file)
-        print('Evaluating {} samples ..'.format(len(data)))
-        jsonOutput = []
-
-        for i,record in enumerate(data):
-            try:
-                print('Processing image - {}'.format(record['imgPath']))
-                file_path = record['imgPath']
-                file_name = os.path.basename(file_path)
-                img = cv2.imread(file_path)
-                H,W,C = img.shape
-
-                # Model Inference..
-                binaryMap,scribbleMap =  imageInference(network,file_path,args,PDIM=256,DIM=256,OVERLAP=0.25,save=True)
-                binaryMap=np.uint8(binaryMap)
-                scribbleMap=np.uint8(scribbleMap)
-
-                scribbles = postProcess(scribbleMap,binaryMap,binaryThreshold=40,rectangularKernel=30)
-                
-                # Storing .. # Put it under vis flag 
-                cv2.imwrite(os.path.join(scr_folder,'scr_'+file_name),scribbleMap)
-                cv2.imwrite(os.path.join(bin_folder,'bin_'+file_name),binaryMap)
-
-
-                # Sending to Stage 2
-                binaryMap = cv2.imread(os.path.join(bin_folder,'bin_'+file_name))
-                scribbleMap = cv2.imread(os.path.join(scr_folder,'scr_'+file_name))
-            
-                ppolygons = imageTask(img,binaryMap,scribbles)
-
-                # Visualise the ppolygons once 
-                img2 = copy.deepcopy(img)
-                for p in ppolygons:
-                    p = np.asarray(p,dtype=np.int32).reshape((-1,1,2))
-                    img2 = cv2.polylines(img2, [p],True, (255, 0, 0),3)
-                cv2.imwrite(os.path.join(vis_folder,'vis_'+file_name),img2)
-
-                # Writing it to JSON file 
-                scrs_ = [ np.asarray(gd).reshape((-1,2)).tolist() for gd in scribbles]
-                pps_ = [ np.asarray(gd).reshape((-1,2)).tolist() for gd in ppolygons]
-
-                jsonOutput.append({'imgPath':record['imgPath'],'imgDims':[H,W],'predScribbles':scrs_,'predPolygons':pps_})
-             
-            except Exception as exp:
-                print('Image :{} Error :{}'.format(file_name,exp))
-                continue
-    
-    # Case II : Image Folder 
     if  args['input_image_folder'] is not None and args['input_image_json'] is None:
         files_ = os.listdir(args['input_image_folder'])
         jsonOutput = []
@@ -312,14 +246,14 @@ def Inference(args):
                     H,W,C = img.shape
                     
                     # Calling Stage I Inference 
-                    binaryMap,scribbleMap =  imageInference(network,file_path,args,PDIM=256,DIM=256,OVERLAP=0.25,save=True)
-                    binaryMap=np.uint8(binaryMap)
+                    scribbleMap =  imageInference(network,file_path,args,PDIM=256,DIM=256,OVERLAP=0.25,save=True)
                     scribbleMap=np.uint8(scribbleMap)
 
                     # Post Processing of Scribble Branch
-                    scribbles = postProcess(scribbleMap,binaryMap,binaryThreshold=40,rectangularKernel=30)
+                    scribbles = postProcess_URDU(scribbleMap,binaryMap)
                     
                     # Visualisation purpose
+                    binaryMap = img.astype('uint8')
                     cv2.imwrite(os.path.join(scr_folder,'scr_'+file_name),scribbleMap)
                     cv2.imwrite(os.path.join(bin_folder,'bin_'+file_name),binaryMap)
 
@@ -357,9 +291,7 @@ def Inference(args):
     with open(os.path.join(args['output_image_folder'],'{}.json'.format(args['exp_name'])),'w') as f:
         json.dump(jsonOutput,f)
     f.close() 
-    print('~Completed Inference !')   
-
-
+    print('~Completed Inference !') 
 
 if __name__ == "__main__":    
     args = addArgs()
